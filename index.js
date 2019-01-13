db488addc0c0d9020541281dafe40592b805a681
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-01-13 19:02:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 // write your code below!
+
 function happyHolidays(string) {
   return "Happy holidays!"
+}
+
+var name
+
+function happyHolidaysTo(string) {
+  return "`Happy Holidays, ${name}`"
 }
